a8ba7932e2c2942107d9798d17b43175be0b5afa
Veronica <Veronica@ASUS>
2020-10-21 09:44:13 +0200

    subida 3

--- src/apps/AppPerrera.java
+++ src/apps/AppPerrera.java
@@ -11,6 +11,7 @@ public class AppPerrera {
 	public static final String OPCION_1 = "1";
 	public static final String OPCION_2 = "2";
 	public static final String OPCION_3 = "3";
+	public static final String OPCION_4 = "4";
 	public static final String SALIR = "s";
 
 	// variables globales para esta Clase
@@ -45,6 +46,9 @@ public class AppPerrera {
 			case OPCION_3:
 				bajaPerro();
 				break;
+			case OPCION_4:
+				updatePerro();
+				break;
 			case SALIR:
 				salir = true;
 				break;
@@ -54,7 +58,7 @@ public class AppPerrera {
 				break;
 			}
 
-		} while (!salir);
+		} while (!salir); // Ó tambien --> while(!SALIR.equalsIgnoreCase(opcion));
 
 		System.out.println("***********  ADIOS, nos vemos pronto   **************");
 		sc.close();
@@ -63,9 +67,64 @@ public class AppPerrera {
 
 	// METODOS
 	/**
+	 * Actualizar un perro. Reemplazar los datos del antiguo perro por otros nuevos.
+	 */
+	private static void updatePerro() {
+		// Pedir nombre perro que se quiere actualizar
+		System.out.println("Escribe el nombre del perro que quieres actualizar:");
+		String nombreActualizar = sc.nextLine();
+
+		// Recorre la lista
+		for (Perro p : lista) {
+			// Si el nombre de la lista es igual al introducido:
+			if (p.getNombre().equals(nombreActualizar)) {
+				// Modificar datos
+				for (int i = 0; i < lista.size(); i++) {
+
+					Perro pIteracion = lista.get(i);
+
+					if (nombreActualizar.equals(pIteracion.getNombre())) {
+						lista.set(index, element);
+						break; // salir del for
+					}
+
+				} // for
+
+			} else {
+				System.out.println("El nombre del perro que has escrito no existe");
+			}
+		}
+
+		/*
+		 * // obtener la posicion de un objeto dentro del array int posicion =
+		 * lista.indexOf(nombreActualizar);
+		 * 
+		 * // Pedir nuevo nombre perro System.out.println("Escribe el nuevo nombre:");
+		 * String nombreNuevo = sc.nextLine();
+		 * 
+		 * // Reemplazar antiguo nombre por el nuevo lista.set(posicion, nombreNuevo);
+		 */
+
+	}
+
+	/**
 	 * Dar de baja a un perro
 	 */
 	private static void bajaPerro() {
+		// Pedir nombre perro que se quiere borrar
+		System.out.println("Escribe el nombre del perro que quieres borrar:");
+		String nombreBorrar = sc.nextLine();
+
+		for (int i = 0; i < lista.size(); i++) {
+
+			Perro pIteracion = lista.get(i);
+
+			if (nombreBorrar.equals(pIteracion.getNombre())) {
+				lista.remove(i);
+				break; // salir del for
+			}
+
+		} // for
 
 	}
 
@@ -95,7 +154,10 @@ public class AppPerrera {
 	private static void listar() {
 
 		for (Perro perro : lista) {
-			System.out.println("Nombre: " + perro.getNombre() + " // " + "Raza: " + perro.getRaza());
+			// Se puede listar de las dos formas:
+			// System.out.println("Nombre: " + perro.getNombre() + " // " + "Raza: " +
+			// perro.getRaza());
+			System.out.println(String.format("%s [%s]", perro.getNombre(), perro.getRaza()));
 		}
 
 	}
@@ -124,6 +186,7 @@ public class AppPerrera {
 		System.out.println(" 1.- Listar todos los perros");
 		System.out.println(" 2.- Crear un perro");
 		System.out.println(" 3.- Dar de baja un Perro");
+		System.out.println(" 4.- Update un Perro (actualizar)");
 		System.out.println(" etc etc ...");
 		System.out.println(" ");
 		System.out.println(" S - Salir");
